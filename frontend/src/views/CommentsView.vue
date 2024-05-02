467fc899d0f8960ebfc793e8fb2fd554d8412a31
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-05-02 12:04:12 +0200

    update allcomment fetch igen after submit

--- frontend/src/views/CommentsView.vue
+++ frontend/src/views/CommentsView.vue
@@ -1,19 +1,28 @@
 <script>
 import CommentForm from '../components/CommentForm.vue'
+import AllComments from '../components/AllComments.vue'
 
 export default {
   components: {
     CommentForm,
+    AllComments,
+  },
+  methods: {
+    updateCommentsEmit() {
+      this.$refs.allComments.fetchComments()
+    },
   },
 }
 </script>
 <template>
   <header>
-    <div class="wrapper"></div>
+    <div class="wrapper">
+      <AllComments ref="allComments" />
+    </div>
   </header>
 
   <main>
-    <CommentForm />
+    <CommentForm @updateCommentsEmit="updateCommentsEmit" />
 
     <router-link name="default" :to="'/'">
       <input type="button" value="Back Home" />
