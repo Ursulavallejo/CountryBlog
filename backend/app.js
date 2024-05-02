e3f55cd7b6000b1b3d2b75ec51c78fe8b63c1bf1
Malin Johansson <malin.johansson2@iths.se>
2024-05-02 13:44:22 +0200

    update malin2

--- backend/app.js
+++ backend/app.js
@@ -16,26 +16,18 @@ app.use(express.static('public'))
 const countryRoutes = require('./routes/countryRoutes')
 const cityRoutes = require('./routes/cityRoutes')
 const languageRoutes = require('./routes/languageRoutes')
-<<<<<<< HEAD
-// const currencyRoutes = require('./routes/currencyRoutes')
-=======
 const currencyRoutes = require('./routes/currencyRoutes')
 
 // Koppling till mongoose
 const commentRoutes = require('./routes/commentRoutes')
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
 
 app.use(countryRoutes)
 app.use(cityRoutes)
 app.use(languageRoutes)
-<<<<<<< HEAD
-// app.use(currencyRoutes)
-=======
 app.use(currencyRoutes)
 app.use(commentRoutes)
 
 const connectionMongoDB = require('./connectionMongoDB')
 connectionMongoDB()
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- backend/controllers/languageController.js
+++ backend/controllers/languageController.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const connectionMySQL = require('../connectionMySQL.js');
-
-exports.getLanguages = async (req, res) => {
-  try {
-    const sql = 'SELECT * FROM language';
-    const results = await connectionMySQL.query(sql);
-    res.json(results);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.createLanguage = async (req, res) => {
-  const { languageId, languageName } = req.body;
-=======
 const connectionMySQL = require('../connectionMySQL.js')
 
 exports.getLanguages = async (req, res) => {
@@ -43,32 +27,10 @@ exports.createLanguage = async (req, res) => {
     languageId,
     languageName,
   ]
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
 
   if (!languageName || languageName.trim().length < 1) {
     return res.status(400).json({
       success: false,
-<<<<<<< HEAD
-      error: 'You have not provided a languageName',
-    });
-  }
-
-  try {
-    const sql = 'INSERT INTO language (languageId, languageName) VALUES (?, ?)';
-    await connectionMySQL.query(sql, [languageId, languageName]);
-    res.status(201).json({
-      success: true,
-      error: '',
-      message: 'You have now added a new language!',
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
-
-exports.updateLanguage = async (req, res) => {
-  const { languageId, languageName } = req.body;
-=======
       error: 'You have not written a languageName',
     })
   }
@@ -102,40 +64,17 @@ exports.updateLanguage = async (req, res) => {
     languageId,
     languageName,
   ]
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
 
   if (!languageName || languageName.trim().length < 1) {
     return res.status(400).json({
       success: false,
-<<<<<<< HEAD
-      error: 'You have not provided any languageName for the country',
-    });
-=======
       error: 'You have not written any language for the country',
     })
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
   }
 
   if (!languageId) {
     return res.status(400).json({
       success: false,
-<<<<<<< HEAD
-      error: 'You have not provided any ID of the language you wish to update!!',
-    });
-  }
-
-  try {
-    const sql = 'UPDATE language SET languageName = ? WHERE languageId = ?';
-    await connectionMySQL.query(sql, [languageName, languageId]);
-    res.json({ success: true, error: '' });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
-
-exports.deleteLanguage = async (req, res) => {
-  const { languageId } = req.body;
-=======
       error: 'You have not provide any ID of the language you wish to update!!',
     })
   }
@@ -160,29 +99,10 @@ exports.deleteLanguage = async (req, res) => {
   const { languageId } = req.body
 
   let sql = 'DELETE FROM language WHERE languageId = ?'
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
 
   if (!languageId) {
     return res.status(400).json({
       success: false,
-<<<<<<< HEAD
-      error: 'You have not provided any ID of the language you wish to delete!',
-    });
-  }
-
-  try {
-    const sql = 'DELETE FROM language WHERE languageId = ?';
-    await connectionMySQL.query(sql, [languageId]);
-    res.json({
-      success: true,
-      error: '',
-      message: 'The language is now deleted!',
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
-=======
       error: 'You have not provide any ID of the language you wish to delete!',
     })
   }
@@ -204,4 +124,3 @@ exports.deleteLanguage = async (req, res) => {
   }
 }
 
->>>>>>> 658ba38bfa7d56a07f4b56260ddc509c2d1d1bf0
